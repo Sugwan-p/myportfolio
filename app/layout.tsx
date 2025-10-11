ecfa55efb70572e7d00284174a17b7f8318d9208
Park Sugwan <456tnrhks@naver.com>
2025-10-12 00:55:22 +0900

    feat : test: clarity test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import IntroScreen from "@/components/templates/IntroScreen";
 import CustomCursor from "@/components/CustomCursor";
@@ -17,15 +18,25 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ko" className="scroll-smooth">
       <body className={notoSansKr.className}>
         <CustomCursor />
         <IntroScreen />
         {children}
+
+        <Script
+          id="ms-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "toj0mpp2at");`,
+          }}
+        />
       </body>
     </html>
   );
